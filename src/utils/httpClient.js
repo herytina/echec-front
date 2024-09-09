4faaeb2fd13072b98ffcac3f5d13d4127768f40b
hery tsitohaina <herytinara8@gmail.com>
2024-09-09 15:26:59 +0200

    add token

--- src/utils/httpClient.js
+++ src/utils/httpClient.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+// import { useStoreUser } from '@/stores/user.store';
 
 export const apiClient = axios.create({
   baseURL: 'http://localhost:3000', // L'URL de base de votre serveur Node.js
@@ -6,5 +7,19 @@ export const apiClient = axios.create({
     'Content-Type': 'application/json'
   }
 });
+
+// // Ajouter un interceptor pour inclure le token à chaque requête
+// apiClient.interceptors.request.use(config => {
+//   const userStore = useStoreUser(); // Accéder au store Pinia
+//   const token = userStore.token; // Récupérer le token depuis le store
+  
+//   if (token) {
+//     config.headers['Authorization'] = `Bearer ${token}`;
+//   }
+  
+//   return config;
+// }, error => {
+//   return Promise.reject(error);
+// });
 
 export const URL = 'http://localhost:3000/api'
